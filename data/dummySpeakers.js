16e73aca22743266eccde40b1bd39cb3a895b017
Jordy V <jordyvcontact@gmail.com>
2022-11-01 11:22:54 -0600

    info added

--- data/dummySpeakers.js
+++ data/dummySpeakers.js
@@ -190,38 +190,19 @@ export const speakers = [
     image: speaker1,
     title: "La industria web3",
     titles: [
-      "C.P & MBA EN FINANZAS",
-      "Chief of Crypto Operations at Soluciones",
-      "Blockchain Mexico",
+      "CEO at Monterrey Bitcoin Embassy",
     ],
-    bio: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-    nostrud exerci tation ullamcorper suscipit lobortis nisl ut
-    aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
-    in hendrerit in vulputate velit esse molestie consequat, vel
-    illum dolore eu feugiat nulla facilisis at vero eros et accumsan
-    et iusto odio dignissim qui blandit praesent luptatum zzril
-    delenit augue duis dolore te feugait nulla facilisi. <br />
-    Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. <br /> <br />
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-    nostrud exerci tation ullamcorper suscipit lobortis nisl ut
-    aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
-    in hendrerit in vulputate velit esse molestie consequat, vel
-    illum dolore eu feugiat nulla facilisis at vero eros et accumsan
-    et iusto odio dignissim qui blandit praesent luptatum zzril
-    delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
-    dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy
-    nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-    volutpat. <br /> Ut wisi enim ad minim veniam, quis nostrud
-    exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
-    ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer
-    adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-    laoreet dolore magna aliquam erat`,
+    bio: `Mtro. Daniel Camacho es profesor investigador, empresario, emprendedor, escritor, conferencista, docente científico, criptoentusiasta, cypherpunk, anarco-economista, tokenomista profesional y contador público. 
+    <br><br>
+    Cuenta los MBA Majors in Finance y Global Management; y una Maestría en Ciencias de la Educación. Tiene más de 12 años de experiencia en auditoría, economía, contabilidad y finanzas; especializado en Industria 4.0, Tecnología Blockchain, Criptodivisas y Tokenomía.
+    Profesor investigador y profesor líder del programa académico de blockchain y tokenomía en el Tecnológico de Monterrey. Fundador y CEO de la Monterrey Bitcoin Embassy. 
+    <br><br>
+    Además, autor de los libros: 
+    <br>Crypto Papers y Tokenomics: una economía tokenizada; 
+    <br>El paper: 
+    <br>Blockchain education: challenging the long-stanidng model of academic institutions; 
+    <br>Y las tesis: 
+    <br>Behavioral Economics: La Neuroeconomía del ahorro y DAO’s Governance: Aplicación de DAO’s en estructuras corporativas.`,
   },
   {
     id: 2,
@@ -262,71 +243,30 @@ export const speakers = [
     id: 3,
     name: "CESAR GAYTAN",
     image: speaker3,
-    title: "La industria web3",
-    titles: ["FOUNDER AND CEO AT KINDYNOS", "Socio co-fundador of bitmon"],
-    bio: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-    nostrud exerci tation ullamcorper suscipit lobortis nisl ut
-    aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
-    in hendrerit in vulputate velit esse molestie consequat, vel
-    illum dolore eu feugiat nulla facilisis at vero eros et accumsan
-    et iusto odio dignissim qui blandit praesent luptatum zzril
-    delenit augue duis dolore te feugait nulla facilisi. <br />
-    Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. <br /> <br />
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-    nostrud exerci tation ullamcorper suscipit lobortis nisl ut
-    aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
-    in hendrerit in vulputate velit esse molestie consequat, vel
-    illum dolore eu feugiat nulla facilisis at vero eros et accumsan
-    et iusto odio dignissim qui blandit praesent luptatum zzril
-    delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
-    dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy
-    nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-    volutpat. <br /> Ut wisi enim ad minim veniam, quis nostrud
-    exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
-    ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer
-    adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-    laoreet dolore magna aliquam erat`,
+    title: "DE BITCOIN A LA WEB3 Y MAS ALLA",
+    titles: ["Founder at Hackwise", ""],
+    bio: `Cesar Gaytán es un empresario Mexicano que ha estado involucrado en el mundo cripto desde hace ya 10 años. 
+    <br>Es conocido por ser uno de los primeros mineros de criptomonedas en Mexico 
+    (<a href='https://youtu.be/UaglRHkBObM' target='_blank'><i style="color:blue">Minando hace casi 10 años</i></a>), por sus constantes aportaciones en el mundo cripto.
+    Es creador de las comunidades: Bitcoin méxico y Blockchain Latam.<br>Entusiasta de blockchain, la ciberseguridad y la Web 3.0.`,
   },
   {
     id: 4,
-    name: "LUIS ENRIQUE CORREA",
+    name: "LUIS DANIEL BELTRAN",
     image: speaker4,
     title: "La industria web3",
-    titles: ["FOUNDER AND CEO AT KINDYNOS", "Socio co-fundador of bitmon"],
-    bio: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-    nostrud exerci tation ullamcorper suscipit lobortis nisl ut
-    aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
-    in hendrerit in vulputate velit esse molestie consequat, vel
-    illum dolore eu feugiat nulla facilisis at vero eros et accumsan
-    et iusto odio dignissim qui blandit praesent luptatum zzril
-    delenit augue duis dolore te feugait nulla facilisi. <br />
-    Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. <br /> <br />
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-    nostrud exerci tation ullamcorper suscipit lobortis nisl ut
-    aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
-    in hendrerit in vulputate velit esse molestie consequat, vel
-    illum dolore eu feugiat nulla facilisis at vero eros et accumsan
-    et iusto odio dignissim qui blandit praesent luptatum zzril
-    delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
-    dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy
-    nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-    volutpat. <br /> Ut wisi enim ad minim veniam, quis nostrud
-    exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
-    ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer
-    adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-    laoreet dolore magna aliquam erat`,
+    titles: ["Co-CEO at Wemerang", ""],
+    bio: `Desde que cumplió 7 años, de manera autodidacta, aprendió a programar. Siendo pionero en el tema de Blockchain creó 
+    <a href='https://pesodigital.org' target='_blank'><i style="color:blue">pesodigital.org</i></a>
+    una iniciativa liderada por ciudadanos para crear una versión completamente digital del peso mexicano. 
+    <br>Así mismo, continúa involucrado en temas de trading algorítmico en el mercado de Forex, Futuros y Criptomonedas. 
+    <br><br>Su pasión por la naturaleza y la vida sostenible lo llevó a construir su casa basada en la permacultura. 
+    Experto en infraestructura y seguridad que lo llevó a fundar 
+    <a href='https://microbit.com' target='_blank'><i style="color:blue">microbit.com,</i></a>
+     un emprendimiento tecnológico enfocado en montar 
+    y administrar infraestructura de seguridad para aplicaciones y sitios web de alto tráfico.
+    <br><br>Es presidente del Nodo Zacatecas de la Red Global MX de Talentos en el Extranjero y miembro fundador del primer espacio de co-working en Zacatecas, Hacker Garage. 
+    <br><br>Actualmente se construye una cripto economía para el planeta, las personas y los animales donde cualquiera puede invertir y ganar dinero movilizando a otros para completar desafíos sociales y ambientales.`,
   },
   {
     id: 5,
@@ -403,35 +343,12 @@ export const speakers = [
     name: "JORGE ANTONIO GALVAN",
     image: speaker7,
     title: "La industria web3",
-    titles: ["FOUNDER AND CEO AT KINDYNOS", "Socio co-fundador of bitmon"],
-    bio: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-    nostrud exerci tation ullamcorper suscipit lobortis nisl ut
-    aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
-    in hendrerit in vulputate velit esse molestie consequat, vel
-    illum dolore eu feugiat nulla facilisis at vero eros et accumsan
-    et iusto odio dignissim qui blandit praesent luptatum zzril
-    delenit augue duis dolore te feugait nulla facilisi. <br />
-    Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. <br /> <br />
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-    nostrud exerci tation ullamcorper suscipit lobortis nisl ut
-    aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
-    in hendrerit in vulputate velit esse molestie consequat, vel
-    illum dolore eu feugiat nulla facilisis at vero eros et accumsan
-    et iusto odio dignissim qui blandit praesent luptatum zzril
-    delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
-    dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy
-    nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-    volutpat. <br /> Ut wisi enim ad minim veniam, quis nostrud
-    exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
-    ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer
-    adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-    laoreet dolore magna aliquam erat`,
+    titles: ["CCO at Vibing Studios.", "GAMING EN WEB3"],
+    bio: `Ing. En Mecatronica egresado del Tec de Monterrey. He trabajado en el desarrollo de videojuegos desde el 2017 en sus diferentes areas, 2D, 3D y realidad virtual. 
+    Gurú tecnológico en ingeniería y arquitectura de juegos. <br>Defensor de la diversión, ante todo. 
+    <br>Con más de 5 años de experiencia en desarrollo y gestión de videojuegos. 
+    <br>Promotor de Play and earn e inversionista en Web3 y actual cofundador de vibing studios.
+    `,
   },
   {
     id: 8,
