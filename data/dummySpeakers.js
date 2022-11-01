dfda5bd5bd3cc9cdba42f5f229e4126c95bd2970
Jordy V <jordyvcontact@gmail.com>
2022-11-01 13:32:36 -0600

    Agenda filled

--- data/dummySpeakers.js
+++ data/dummySpeakers.js
@@ -6,6 +6,11 @@ import speaker5 from "../assets/images/speaker5.png";
 import speaker6 from "../assets/images/speaker6.png";
 import speaker7 from "../assets/images/speaker7.png";
 import boogle from "../assets/images/boogle.png";
+import emp from "../assets/images/emp.png";
+import tallerF from "../assets/images/tallerfuturo.png";
+import masterC from "../assets/images/mc.png";
+import black from "../assets/images/black.png";
+import f3 from "../assets/images/f3.png";
 
 export const dummySpeakers = [
   {
@@ -13,56 +18,81 @@ export const dummySpeakers = [
     date: "1 Dic 2022",
     speakers: [
       {
-        name: "DANIEL CAMACHO",
-        img: speaker1,
-        title: "La industria web3",
+        name: "Ricardo Bustos",
+        img: boogle,
+        title: "INAUGURACIÓN Y PRESENTACIÓN VIBING.",
         titles: [
-          "C.P & MBA EN FINANZAS",
-          "Chief of Crypto Operations at Soluciones",
-          "Blockchain Mexico",
+          "CEO at Vibing Studios.",
         ],
         event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
+          "<b>INAUGURACIÓN Y PRESENTACIÓN VIBING.</b>",
+        time: "10:00 am",
       },
       {
-        name: "LUIS DANIEL BELTRAN",
-        img: speaker2,
-        title: "La industria web3",
-        titles: [
-          "FOUNDER AND CEO AT MICROBIT",
-          "Socio co-fundador of wemerang",
-        ],
-        event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
-      },
-      {
-        name: "ENRIQUE BERRETUA",
-        img: speaker3,
-        title: "La industria web3",
-        titles: ["FOUNDER AND CEO AT KINDYNOS", "Socio co-fundador of bitmon"],
-        event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
-      },
-      {
-        name: "CESAR GAYTAN",
+        name: "César Gaytan",
         img: speaker4,
-        title: "La industria web3",
-        titles: ["FOUNDER AND CEO AT KINDYNOS", "Socio co-fundador of bitmon"],
+        title: "DE BITCOIN A LA WEB3 Y MAS ALLÁ",
+        titles: [
+          "Founder and CEO at microbit",
+          "Wemerang co-founder",
+        ],
         event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
+          "<b>DE BITCOIN A LA WEB3 Y MAS ALLÁ</b>",
+        time: "11:00 am",
       },
       {
-        name: "LUIS ENRIQUE CORREA",
+        name: "Emprendimiento 1",
+        img: emp,
+        title: "",
+        titles: ["Parque de Innovación"],
+        event:
+          "<b>REALIDAD VIRTUAL EN LA F1</b>",
+        time: "12:00 pm",
+      },
+      {
+        name: "Taller Futuro3",
+        img: tallerF,
+        title: "La industria web3",
+        titles: ["Vibing."],
+        event:
+          "<b>NFT Utility Showcase</b>",
+        time: "1:00 pm - 2:00 pm",
+      },
+      {
+        name: "Luis Correa",
         img: speaker5,
         title: "La industria web3",
         titles: ["CTO AT VIBING.", "Blockchain Developer."],
         event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
+          "<b>BITCOIN, LA EVOLUCION DEL DINERO.</b>",
+        time: "3:00 pm",
+      },
+      {
+        name: "Abimelek Casterezana",
+        img: speaker6,
+        title: "La industria web3",
+        titles: ["Blockchain developer & influencer."],
+        event:
+          "<b>EL METAVERSO VIENE Y ES SENSACIONAL.</b>",
+        time: "4:00 pm",
+      },
+      {
+        name: "Daniel Camacho",
+        img: speaker3,
+        title: "La industria web3",
+        titles: ["Chief of Crypto Operations.", "at Soluciones Blockchain Mexico"],
+        event:
+          "<b>COMO LLEVAR TU EMPRESA A LA WEB3.</b>",
+        time: "5:00 pm",
+      },
+      {
+        name: "Masterclass 1",
+        img: masterC,
+        title: "La industria web3",
+        titles: ["Daniel Camacho."],
+        event:
+          "<b>MASTERCLASS 1 <br> INTRODUCCIÓN A BLOCKCHAIN</b>",
+        time: "6:30 pm - 10:30 pm",
       },
     ],
   },
@@ -71,114 +101,97 @@ export const dummySpeakers = [
     date: "2 Dic 2022",
     speakers: [
       {
-        name: "DANIEL CAMACHO",
+        name: "Fernando Sosa",
+        img: black,
+        title: "La industria web3",
+        titles: [
+          "Tribal",
+        ],
+        event:
+          "<b>CRYPTO EN FINTECH</b>",
+        time: "10:00 am",
+      },
+      {
+        name: "Luis Daniel Beltran",
         img: speaker1,
         title: "La industria web3",
         titles: [
-          "C.P & MBA EN FINANZAS",
-          "Chief of Crypto Operations at Soluciones",
-          "Blockchain Mexico",
+          "Founder of Wemerang",
         ],
         event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
+          "<b>BLOCKCHAIN, ¿SÍ CAMBIÓ AL MUNDO?</b>",
+        time: "11:00 am",
       },
       {
-        name: "LUIS DANIEL BELTRAN",
-        img: speaker2,
-        title: "La industria web3",
-        titles: [
-          "FOUNDER AND CEO AT MICROBIT",
-          "Socio co-fundador of wemerang",
-        ],
-        event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
-      },
-      {
-        name: "ENRIQUE BERRETUA",
+        name: "Daniel Camacho",
         img: speaker3,
         title: "La industria web3",
-        titles: ["FOUNDER AND CEO AT KINDYNOS", "Socio co-fundador of bitmon"],
+        titles: ["Chief of Crypto Operations.", "at Soluciones Blockchain Mexico"],
         event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
+          "<b>TOKENOMICS: TOKENIZANDO Y NFTIZANDO</b>",
+        time: "12:00 pm",
       },
       {
-        name: "CESAR GAYTAN",
-        img: speaker4,
+        name: "Emprendimiento 2",
+        img: emp,
         title: "La industria web3",
-        titles: ["FOUNDER AND CEO AT KINDYNOS", "Socio co-fundador of bitmon"],
+        titles: ["Parque de Innovación"],
         event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
+          "<b>PREPAVERSO</b>",
+        time: "1:00 pm - 2:00 pm",
       },
       {
-        name: "LUIS ENRIQUE CORREA",
-        img: speaker5,
+        name: "Enrique Berrueta",
+        img: speaker2,
         title: "La industria web3",
-        titles: ["CTO AT VIBING.", "Blockchain Developer."],
+        titles: ["CEO AT Kyndinos"],
         event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
+          "<b>WEB3, LA EVOLUCIÓN DEL INTERNET</b>",
+        time: "3:00 pm",
+      },
+      {
+        name: "Jorge Galván",
+        img: speaker7,
+        title: "La industria web3",
+        titles: ["COO AT Vibing Studios"],
+        event:
+          "<b>GAMING EN WEB3</b>",
+        time: "4:00 pm",
+      },
+      {
+        name: "Future3 Panel",
+        img: f3,
+        title: "La industria web3",
+        titles: ["Multiple awesome people"],
+        event:
+          "<b>PANEL: EL MUNDO DECENTRALIZADO</b><br><i>Ricardo Bustos, Daniel Camacho, Enrique Berrueta y Luis Beltrán</i>",
+        time: "5:00 pm",
+      },
+      {
+        name: "Masterclass 2",
+        img: masterC,
+        title: "La industria web3",
+        titles: ["Daniel Camacho"],
+        event:
+          "LA TOKENIZACION DE LAS COSAS",
+        time: "6:30 pm - 10:00 pm",
       },
     ],
   },
   {
     id: 3,
-    date: "MASTERCLASS",
+    date: "3 DIC 2022",
     speakers: [
       {
-        name: "DANIEL CAMACHO",
-        img: speaker1,
+        name: "Masterclass 3",
+        img: masterC,
         title: "La industria web3",
         titles: [
-          "C.P & MBA EN FINANZAS",
-          "Chief of Crypto Operations at Soluciones",
-          "Blockchain Mexico",
+          "Daniel Camacho",
         ],
         event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
-      },
-      {
-        name: "LUIS DANIEL BELTRAN",
-        img: speaker2,
-        title: "La industria web3",
-        titles: [
-          "FOUNDER AND CEO AT MICROBIT",
-          "Socio co-fundador of wemerang",
-        ],
-        event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
-      },
-      {
-        name: "ENRIQUE BERRETUA",
-        img: speaker3,
-        title: "La industria web3",
-        titles: ["FOUNDER AND CEO AT KINDYNOS", "Socio co-fundador of bitmon"],
-        event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
-      },
-      {
-        name: "CESAR GAYTAN",
-        img: speaker4,
-        title: "La industria web3",
-        titles: ["FOUNDER AND CEO AT KINDYNOS", "Socio co-fundador of bitmon"],
-        event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
-      },
-      {
-        name: "LUIS ENRIQUE CORREA",
-        img: speaker5,
-        title: "La industria web3",
-        titles: ["CTO AT VIBING.", "Blockchain Developer."],
-        event:
-          "Web2 open finance data + Web3 smart <br /> contacts = Financial automation",
-        time: "11:00",
+          "CREACIÓN Y APLICACIÓN DE SMART CONTRACTS",
+        time: "10:00 am - 2:00 pm",
       },
     ],
   },
@@ -187,6 +200,41 @@ export const dummySpeakers = [
 export const speakers = [
   {
     id: 1,
+    name: "Ricardo Bustos",
+    image: boogle,
+    title: "Inauguración",
+    titles: ["FOUNDER AND CEO AT KINDYNOS", "Socio co-fundador of bitmon"],
+    bio: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+    nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+    aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
+    in hendrerit in vulputate velit esse molestie consequat, vel
+    illum dolore eu feugiat nulla facilisis at vero eros et accumsan
+    et iusto odio dignissim qui blandit praesent luptatum zzril
+    delenit augue duis dolore te feugait nulla facilisi. <br />
+    Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed
+    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+    aliquam erat volutpat. <br /> <br />
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
+    nostrud exerci tation ullamcorper suscipit lobortis nisl ut
+    aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
+    in hendrerit in vulputate velit esse molestie consequat, vel
+    illum dolore eu feugiat nulla facilisis at vero eros et accumsan
+    et iusto odio dignissim qui blandit praesent luptatum zzril
+    delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
+    dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy
+    nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+    volutpat. <br /> Ut wisi enim ad minim veniam, quis nostrud
+    exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
+    ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer
+    adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+    laoreet dolore magna aliquam erat`,
+  },
+  {
+    id: 2,
     name: "Daniel Camacho",
     image: speaker3,
     title: "La industria web3",
@@ -206,7 +254,7 @@ export const speakers = [
     <br>Behavioral Economics: La Neuroeconomía del ahorro y DAO’s Governance: Aplicación de DAO’s en estructuras corporativas.`,
   },
   {
-    id: 2,
+    id: 3,
     name: "Enrique Berrueta",
     image: speaker2,
     title: "La industria web3",
@@ -241,7 +289,7 @@ export const speakers = [
     laoreet dolore magna aliquam erat`,
   },
   {
-    id: 3,
+    id: 4,
     name: "Cesar Gaytan",
     image: speaker4,
     title: "De Bitcoin a la Web3 y mas allá",
@@ -252,7 +300,7 @@ export const speakers = [
     Es creador de las comunidades: Bitcoin méxico y Blockchain Latam.<br>Entusiasta de blockchain, la ciberseguridad y la Web 3.0.`,
   },
   {
-    id: 4,
+    id: 5,
     name: "Luis Daniel Beltran",
     image: speaker1,
     title: "La industria web3",
@@ -270,44 +318,9 @@ export const speakers = [
     <br><br>Actualmente se construye una cripto economía para el planeta, las personas y los animales donde cualquiera puede invertir y ganar dinero movilizando a otros para completar desafíos sociales y ambientales.`,
   },
   {
-    id: 5,
+    id: 6,
     name: "Luis Enrique Correa",
     image: speaker5,
-    title: "La industria web3",
-    titles: ["FOUNDER AND CEO AT KINDYNOS", "Socio co-fundador of bitmon"],
-    bio: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-    nostrud exerci tation ullamcorper suscipit lobortis nisl ut
-    aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
-    in hendrerit in vulputate velit esse molestie consequat, vel
-    illum dolore eu feugiat nulla facilisis at vero eros et accumsan
-    et iusto odio dignissim qui blandit praesent luptatum zzril
-    delenit augue duis dolore te feugait nulla facilisi. <br />
-    Lorem ipsum dolor sit amet, cons ectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. <br /> <br />
-    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-    diam nonummy nibh euismod tincidunt ut laoreet dolore magna
-    aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-    nostrud exerci tation ullamcorper suscipit lobortis nisl ut
-    aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
-    in hendrerit in vulputate velit esse molestie consequat, vel
-    illum dolore eu feugiat nulla facilisis at vero eros et accumsan
-    et iusto odio dignissim qui blandit praesent luptatum zzril
-    delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum
-    dolor sit amet, cons ectetuer adipiscing elit, sed diam nonummy
-    nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-    volutpat. <br /> Ut wisi enim ad minim veniam, quis nostrud
-    exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex
-    ea commodo consequat. Lorem ipsum dolor sit amet, consectetuer
-    adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
-    laoreet dolore magna aliquam erat`,
-  },
-  {
-    id: 6,
-    name: "Ricardo Bustos",
-    image: boogle,
     title: "La industria web3",
     titles: ["FOUNDER AND CEO AT KINDYNOS", "Socio co-fundador of bitmon"],
     bio: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
